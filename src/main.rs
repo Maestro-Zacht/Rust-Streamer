600a80ee27c1f2c94271bf1749125d08a12c6382
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2024-07-17 12:35:33 +0200

    testing code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use rust_streamer::streaming::Streaming;
+
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Send,
+    Recv(ServerArgs),
+}
+
+#[derive(Args, Debug)]
+struct ServerArgs {
+    ip: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let streaming = match cli.command {
+        Commands::Send => Streaming::new_server().unwrap(),
+        Commands::Recv(ServerArgs { ip }) => Streaming::new_client(ip).unwrap(),
+    };
+
+    match streaming {
+        Streaming::Client(client) => {
+            client.start().unwrap();
+            println!("Client started");
+            std::thread::sleep(std::time::Duration::from_secs(100));
+            client.stop().unwrap();
+        }
+        Streaming::Server(server) => {
+            server.start().unwrap();
+            println!("Server started");
+            std::thread::sleep(std::time::Duration::from_secs(100));
+            server.close().unwrap();
+        }
+    }
 }
--- src/streaming.rs
+++ src/streaming.rs
@@ -1,2 +1,17 @@
 pub mod client;
 pub mod server;
+
+pub enum Streaming {
+    Client(client::StreamingClient),
+    Server(server::StreamingServer),
+}
+
+impl Streaming {
+    pub fn new_client<T: AsRef<str>>(ip: T) -> Result<Self, client::StreamingClientError> {
+        client::StreamingClient::new(ip).map(Streaming::Client)
+    }
+
+    pub fn new_server() -> Result<Self, server::StreamingServerError> {
+        server::StreamingServer::new().map(Streaming::Server)
+    }
+}
